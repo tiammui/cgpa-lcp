7aa311c011b9ce943a83e93806d4633dee3e9507
tiammui <48317927+tiammui@users.noreply.github.com>
2023-04-20 16:31:41 +0100

    home ui - gpa screen

--- src/components/CalcTypeGPA.js
+++ src/components/CalcTypeGPA.js
@@ -6,14 +6,16 @@ import {
   faShoppingCart,
   faUserAlt,
   faUserCircle,
-  faSignOut
+  faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
 
 export default function () {
-
   return (
     <div id="">
       GPA here
+      <div className="note-card">
+        <h3>GPA</h3>
+      </div>
     </div>
   );
 }
