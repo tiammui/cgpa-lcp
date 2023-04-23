19a35d656daf22a27c5510fa2e8bc36db5b62115
Tiamiyu Muizz <48317927+tiammui@users.noreply.github.com>
2023-04-24 00:48:02 +0100

    home ui- input

--- src/components/CalcTypeGPA.js
+++ src/components/CalcTypeGPA.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
   faShoppingCart,
   faUserAlt,
   faUserCircle,
   faSignOut,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
+
+import { InputCon,Spacer } from "./components";
 
 export default function () {
   return (
@@ -17,10 +19,11 @@ export default function () {
         <p>GPA calculation only take one semester into consideration.</p>
         <p>CGPA calculation takes multiple semester into consideration.</p>
       </div>
-      <div className='pad-1'>
+      <div className="pad-1">
         <h3>FOR LCP STUDENTS</h3>
         <p>If you're not a student of LCP click here</p>
-
+        <Spacer axis="y" spaceRatio={3}></Spacer>
+        <InputCon inputType="text" placeholder="Choose dept" label="Lopuu" />
       </div>
     </div>
   );
--- src/components/components.js
+++ src/components/components.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { NumericFormat } from 'react-number-format';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { NumericFormat } from "react-number-format";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
   faChevronRight,
@@ -9,17 +9,122 @@ import {
   faCaretRight,
   faEllipsisH,
   faTimes,
-} from '@fortawesome/free-solid-svg-icons';
-import { nanoid } from 'nanoid';
+} from "@fortawesome/free-solid-svg-icons";
+import { nanoid } from "nanoid";
 
-export function Spacer({ axis, space }) {
+export function Spacer({ axis, spaceRatio }) {
+  /**
+   *
+   * @param {number} ratio relative to standard-spacing
+   */
+  function cssVar(ratio) {
+    switch (ratio) {
+      case 1:
+        return "var(--space)";
+      case 2:
+        return "var(--space2)";
+      case 3:
+        return "var(--space3)";
+      case 4:
+        return "var(--space4)";
+      default:
+        return "0px";
+    }
+  }
+
   return (
     <div
       style={{
-        display: !axis || axis == 'x' ? 'inline-block' : 'block',
-        height: !axis || axis == 'x' ? '0px' : space + 'px',
-        width: !axis || axis == 'x' ? space + 'px' : '0px',
+        display: !axis || axis == "x" ? "inline-block" : "block",
+        height: !axis || axis == "x" ? "0px" : cssVar(spaceRatio),
+        width: !axis || axis == "x" ? cssVar(spaceRatio) : "0px",
       }}
     ></div>
+  );
+}
+
+const departmentOptions = [
+  { value: "", text: "Choose Department" },
+  { value: "cs", text: "Computer Science" },
+  { value: "cs", text: "Computer Science" },
+];
+const programOptions = [
+  { value: "", text: "Choose program" },
+  { value: "nd-pt", text: "ND Part-time" },
+  { value: "nd-pt", text: "ND Part-time" },
+];
+const semesterOptions = [
+  { value: "", text: "Choose semester" },
+  { value: "4", text: "4th Semester (Year II 1st semester)" },
+  { value: "4", text: "4th Semester (Year II 1st semester)" },
+];
+
+export function InputCon({
+  label,
+  placeholder,
+  inputType,
+  selectType,
+  defaultValue,
+}) {
+  return (
+    <div class="input-con">
+      <label for="avatar-name">{label}</label>
+      {/* <!-- <div class="input">
+<input type="text" id="avatar-name" name="avatar-name" required>
+</div> --> */}
+      {(inputType == "select" && (
+        <SelectInput selectType={selectType} defaultValue={defaultValue} />
+      )) || <TextInput placeholder={placeholder} defaultValue={defaultValue} />}
+      <ul id="avatar-name-error" class="error">
+        <li>Only alphabets are allowed</li>
+        <li>Minimum of 3 characters, Maximum of 20 character</li>
+      </ul>
+    </div>
+  );
+}
+
+function TextInput({ placeholder, defaultValue }) {
+  return (
+    <input
+      type="text"
+      id="avatar-name"
+      name="avatar-name"
+      placeholder={placeholder}
+      value={defaultValue}
+      aria-invalid="false"
+      aria-errormessage="avatar-name-error"
+    />
+  );
+}
+function SelectInput({ selectType, defaultValue }) {
+  function renderOptions(selectType) {
+    switch (selectType) {
+      case "department":
+        return departmentOptions.map((option) => (
+          <option value={option.value}>{option.text}</option>
+        ));
+      case "program":
+        return programOptions.map((option) => (
+          <option value={option.value}>{option.text}</option>
+        ));
+      case "semester":
+        return semesterOptions.map((option) => (
+          <option value={option.value}>{option.text}</option>
+        ));
+      default:
+        return <option value="">Nothing</option>;
+    }
+  }
+
+  return (
+    <select
+      id="avatar-name"
+      name="avatar-name"
+      value={defaultValue}
+      aria-invalid="false"
+      aria-errormessage="avatar-name-error"
+    >
+      {renderOptions(selectType)}
+    </select>
   );
 }
